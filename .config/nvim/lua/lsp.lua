b2cc90000f8d628bda06ef98274056bc9da4c3a9
Lucas Sta Maria <lucas@priime.dev>
2022-10-17 21:28:10 -0400

    fix: use non-deprecated method for capabilities
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -60,7 +60,7 @@ cmp.setup.cmdline(':', {
     })
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local on_attach = function(client, bufnr)
     local opts = { noremap = true, silent = true }
