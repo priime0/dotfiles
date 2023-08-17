1f24a307804b791c80395dec2506318d6e94a0a9
Lucas Sta Maria <lucas@priime.dev>
2023-08-17 00:54:40 -0700

    fix: single space sentences
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- .emacs.d/include/general.el
+++ .emacs.d/include/general.el
@@ -71,6 +71,11 @@
 ;; Undos
 (setq undo-limit 200000)
 
+;; Sentences
+;; Emacs thinks that sentences ends with period and two spaces,
+;; disable this.
+(setq sentence-end-double-space nil)
+
 ;; Theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'nano t)
