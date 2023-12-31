aa4f5159349b6079e1db7669907d2656ac882bd2
Lucas Sta Maria <lucas@priime.dev>
2023-12-31 09:08:43 -0500

    fix(emacs): remove `hledger-mode` fluff
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- .emacs.d/include/misc.el
+++ .emacs.d/include/misc.el
@@ -91,15 +91,13 @@
 (setq hledger-currency-string "$")
 (add-to-list 'auto-mode-alist '("\\.journal\\'" . hledger-mode))
 (setq hledger-jfile "~/finance/2024.journal")
-(add-to-list 'company-backends 'hledger-company)
-(add-to-list 'ac-modes 'hledger-mode)
-(add-hook 'hledger-mode-hook
-    (lambda ()
-        (setq-local ac-sources '(hledger-ac-source))))
 
 
 ;; SLIME
 (setq inferior-lisp-program "sbcl")
+
+;; Copilot
+(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
 
 (provide 'misc)
 ;;; misc.el ends here
