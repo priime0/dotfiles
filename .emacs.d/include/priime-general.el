d66a9cd13ca146f57c681ec5c3fc7ab5e0221b83
Lucas Sta Maria <lucas@priime.dev>
2024-11-04 19:30:31 -0500

    fix(emacs): add missing f6, f7 keybinds
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- .emacs.d/include/priime-general.el
+++ .emacs.d/include/priime-general.el
@@ -140,7 +140,9 @@
 
   :bind
   (("C-v" . View-scroll-half-page-forward)
-   ("M-v" . View-scroll-half-page-backward))
+   ("M-v" . View-scroll-half-page-backward)
+   ("<f6>" . priime-split-right)
+   ("<f7>" . priime-split-down))
 
   :hook
   ((prog-mode . priime-display-relative))
