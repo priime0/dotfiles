ec156560386e367b86e6f4bfb8fedfaac5986b7c
Lucas Sta Maria <lucas@priime.dev>
2024-11-10 20:32:23 -0500

    fix(emacs): enable clippy for eglot
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- .emacs.d/include/priime-lsp.el
+++ .emacs.d/include/priime-lsp.el
@@ -5,7 +5,11 @@
 
 (use-package eglot :straight t
   :custom-face (eglot-inlay-hint-face ((t (:height 1.0))))
-  :hook ((racket-mode rust-mode irony-mode) . eglot-ensure))
+  :hook ((racket-mode rust-mode irony-mode) . eglot-ensure)
+  :init
+  (add-to-list 'eglot-server-programs
+               '((rust-mode rust-ts-mode)
+                 ("rust-analyzer" :initializationOptions (:checkOnSave (:command "clippy"))))))
 (use-package eldoc :straight t
   :after (eglot))
 (use-package eldoc-box :straight t
