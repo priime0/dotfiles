d7908a89fbeb18cff55515b7a1fa9841f0b967ce
Lucas Sta Maria <lucas@priime.dev>
2024-11-10 22:05:19 -0500

    fix(emacs): add keymap for eglot
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- .emacs.d/include/priime-lsp.el
+++ .emacs.d/include/priime-lsp.el
@@ -3,9 +3,15 @@
 ;; Provides configuration for LSP-related packages.
 ;;; Code:
 
+(defvar-keymap priime-eglot-keymap
+  "r" #'eglot-rename
+  "a" #'eglot-code-actions
+  "f" #'eglot-format)
+
 (use-package eglot :straight t
   :custom-face (eglot-inlay-hint-face ((t (:height 1.0))))
   :hook ((racket-mode rust-mode irony-mode) . eglot-ensure)
+  :bind-keymap ("C-c l" . priime-eglot-keymap)
   :init
   (add-to-list 'eglot-server-programs
                '((rust-mode rust-ts-mode)
