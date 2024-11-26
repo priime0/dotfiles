9a42f6ba48bb4e84e5c31f0a50e77cb0afa5d6b2
Lucas Sta Maria <lucas@priime.dev>
2024-11-25 19:19:18 -0500

    feat(emacs): add `git-link` and new function `git-link-with-commit`
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- .emacs.d/include/priime-vc.el
+++ .emacs.d/include/priime-vc.el
@@ -11,6 +11,19 @@
          ("C-x g" . magit-status)))
 (use-package forge :straight t
   :after (magit))
+(use-package git-link :straight t)
+
+(defun git-link-with-commit ()
+  "Create a URL representing the current selection for the current commit."
+  (interactive)
+  (let* ((link-region (git-link--get-region))
+         (old-link (git-link (git-link--remote) (car link-region) (cadr link-region)))
+         (branch (git-link--branch))
+         (commit (git-link--commit))
+         (new-link (s-replace branch commit old-link)))
+    (kill-new new-link)
+    (message new-link)
+    new-link))
 
 (provide 'priime-vc)
 
