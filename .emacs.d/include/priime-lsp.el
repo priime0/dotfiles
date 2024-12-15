816fd80010ca1f133b62eaca8bfa3da679139741
Lucas Sta Maria <lucas@priime.dev>
2024-12-15 01:39:10 -0500

    fix(emacs): improve eglot performance
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- .emacs.d/include/priime-lsp.el
+++ .emacs.d/include/priime-lsp.el
@@ -4,12 +4,19 @@
 ;;; Code:
 
 (use-package eglot :straight t
+  :custom
+  (eglot-events-buffer-config '(:size 0 :format full))
   :custom-face (eglot-inlay-hint-face ((t (:height 1.0))))
   :hook ((racket-mode rust-mode irony-mode) . eglot-ensure)
   :bind (:map eglot-mode-map
          ("C-c l r" . eglot-rename)
          ("C-c l a" . eglot-code-actions)
-         ("C-c l f" . eglot-format)))
+         ("C-c l f" . eglot-format))
+  :init (fset #'jsonrpc--log-event #'ignore))
+(use-package eglot-booster
+  :straight (eglot-booster :type git :host github :repo "jdtsmith/eglot-booster")
+  :after (eglot)
+  :config (eglot-booster-mode))
 (use-package eldoc :straight t
   :after (eglot))
 (use-package eldoc-box :straight t
