b5bc7fe871fb32b66ed167273d9fe4c762a136ce
Lucas Sta Maria <lucas@priime.dev>
2025-02-21 00:01:34 -0500

    fix(emacs): expand eglot, add nix lsp

--- .emacs.d/include/priime-lsp.el
+++ .emacs.d/include/priime-lsp.el
@@ -7,12 +7,14 @@
   :custom
   (eglot-events-buffer-config '(:size 0 :format full))
   :custom-face (eglot-inlay-hint-face ((t (:height 1.0))))
-  :hook ((racket-mode rust-mode irony-mode) . eglot-ensure)
+  :hook ((racket-mode rust-mode irony-mode tuareg-mode python-mode tsx-ts-mode nix-mode) . eglot-ensure)
   :bind (:map eglot-mode-map
          ("C-c l r" . eglot-rename)
          ("C-c l a" . eglot-code-actions)
          ("C-c l f" . eglot-format))
-  :init (fset #'jsonrpc--log-event #'ignore))
+  :init
+  (fset #'jsonrpc--log-event #'ignore)
+  (add-to-list 'eglot-server-programs '(nix-mode "nil")))
 (use-package eglot-booster
   :straight (eglot-booster :type git :host github :repo "jdtsmith/eglot-booster")
   :after (eglot)
